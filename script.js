391801c9acedfdb4229b31b4c41411ebb5123046
tsunetoki <hi-g.g.908@softbank.ne.jp>
2016-06-18 15:58:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function onButtonClick1() {
+    var input = document.getElementById("id_textBox1").value;
+    var items = input.split(" ");
+
+    var outControl = document.getElementById("output");
+    for (var i = 0; i < items.length; i++) {
+        outControl.value += items[i] + "\r\n";
+    }
+}
+
+array.forEach(function(elem, index, arr){
+});
